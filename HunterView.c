9bf75a8d3fd1030a2449c6ecd5642b5062a815f8
Jerryfish <bell.jezz@gmail.com>
2013-10-04 17:33:13 +1000

    Started updating game from pastPlays

--- HunterView.c
+++ HunterView.c
@@ -3,6 +3,7 @@
 #include "game.h"
 #include "HunterView.h"
 #include <stdio.h>
+#include <string.h>
 
 #define ROAD 0
 #define RAIL 1
@@ -35,7 +36,7 @@ static void showGraph(Graph g);
 //returns 1 if there is an edge from start to end with a given transport type
 //returns 0 otherwise
 static int isAdjacent(Graph g,Location start, Location end, Transport type);
-
+static void update(HunterView hunterView, char *play);
 typedef struct vNode *VList;
 
 static void makeMap(Graph g);
@@ -57,21 +58,23 @@ struct GraphRep {
      
 struct hunterView {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    int curPlayer;
-    int round;
+    int curPlayer; //Player whose turn it is
+    int round; //Round number
     int score;
-    int hp[5];
-    char *pastPlays;
-    int playerLocations[5];
-    LocationID playerLocation;
-    int hello;
-    Graph map;
+    int hp[5]; //The hunters health and dracula's blood points
+    char *pastPlays; //All plays ever made
+    int playerLocations[5]; //Location of all 5 players
+    LocationID playerLocation; //Location of current player
+    int hello; //Whatever that is
+    Graph map; //The graph of the map
+    char trail[6][2]; //An array of draculas trail and traps/vampires
 };
      
 
 HunterView newHunterView( char *pastPlays, playerMessage messages[] ) {
     HunterView hunterView = malloc( sizeof( struct hunterView ) );
-    
+   
+    //Put the initial values into the hunterView adt
     hunterView->hello = 42;
     hunterView->curPlayer = 0;
     hunterView->round = 0;
@@ -91,13 +94,144 @@ HunterView newHunterView( char *pastPlays, playerMessage messages[] ) {
     hunterView->playerLocations[3] = UNKNOWN_LOCATION;
     hunterView->playerLocations[4] = UNKNOWN_LOCATION;
     
+    //Store each characters information in a play
+    char play[7];
+    int i1 = 0;
+    int i2 = 0;
+
+    while (pastPlays[i1] != '\0')  {
+        //Build the string with the player info for this turn
+        play[i2] = pastPlays[i1];
+        if (i2 == 7) {
+            //When the string is full update the hunterView
+            update(hunterView, play);
+        }
+        i1 ++;
+        i2 = i1%7;
+
+    }
+
+    update(hunterView, pastPlays);
     hunterView->map = newGraph();
     
     showGraph(hunterView->map);
     return hunterView;
 }
-     
-     
+
+static void update(HunterView hunterView, char *play) {
+    
+    //The character whose turn it is
+    int character = play[0];
+
+    //A string to store the city code
+    char city[3];
+    city[0] = play[1];
+    city[1] = play[2];
+    city[2] = '\n';
+    
+    //Move the player to the new location
+    if (strcmp(city,"AL") == 0) hunterView->playerLocations[character] = 0;
+    if (strcmp(city,"AM") == 0) hunterView->playerLocations[character] = 1;
+    if (strcmp(city,"AT") == 0) hunterView->playerLocations[character] = 2;
+    if (strcmp(city,"BA") == 0) hunterView->playerLocations[character] = 3;
+    if (strcmp(city,"BI") == 0) hunterView->playerLocations[character] = 4;
+    if (strcmp(city,"BE") == 0) hunterView->playerLocations[character] = 5;
+    if (strcmp(city,"BR") == 0) hunterView->playerLocations[character] = 6;
+    if (strcmp(city,"BO") == 0) hunterView->playerLocations[character] = 7;
+    if (strcmp(city,"BU") == 0) hunterView->playerLocations[character] = 8;
+    if (strcmp(city,"BC") == 0) hunterView->playerLocations[character] = 9;
+    if (strcmp(city,"BD") == 0) hunterView->playerLocations[character] = 10;
+    if (strcmp(city,"CA") == 0) hunterView->playerLocations[character] = 11;
+    if (strcmp(city,"CG") == 0) hunterView->playerLocations[character] = 12;
+    if (strcmp(city,"CD") == 0) hunterView->playerLocations[character] = 13;
+    if (strcmp(city,"CF") == 0) hunterView->playerLocations[character] = 14;
+    if (strcmp(city,"CO") == 0) hunterView->playerLocations[character] = 15;
+    if (strcmp(city,"CN") == 0) hunterView->playerLocations[character] = 16;
+    if (strcmp(city,"DU") == 0) hunterView->playerLocations[character] = 17;
+    if (strcmp(city,"ED") == 0) hunterView->playerLocations[character] = 18;
+    if (strcmp(city,"FL") == 0) hunterView->playerLocations[character] = 19;
+    if (strcmp(city,"FR") == 0) hunterView->playerLocations[character] = 20;
+    if (strcmp(city,"GA") == 0) hunterView->playerLocations[character] = 21;
+    if (strcmp(city,"GW") == 0) hunterView->playerLocations[character] = 22;
+    if (strcmp(city,"GE") == 0) hunterView->playerLocations[character] = 23;
+    if (strcmp(city,"GO") == 0) hunterView->playerLocations[character] = 24;
+    if (strcmp(city,"GR") == 0) hunterView->playerLocations[character] = 25;
+    if (strcmp(city,"HA") == 0) hunterView->playerLocations[character] = 26;
+    if (strcmp(city,"JM") == 0) hunterView->playerLocations[character] = 27;
+    if (strcmp(city,"KL") == 0) hunterView->playerLocations[character] = 28;
+    if (strcmp(city,"LE") == 0) hunterView->playerLocations[character] = 29;
+    if (strcmp(city,"LI") == 0) hunterView->playerLocations[character] = 30;
+    if (strcmp(city,"LS") == 0) hunterView->playerLocations[character] = 31;
+    if (strcmp(city,"LV") == 0) hunterView->playerLocations[character] = 32;
+    if (strcmp(city,"LO") == 0) hunterView->playerLocations[character] = 33;
+    if (strcmp(city,"MA") == 0) hunterView->playerLocations[character] = 34;
+    if (strcmp(city,"MN") == 0) hunterView->playerLocations[character] = 35;
+    if (strcmp(city,"MR") == 0) hunterView->playerLocations[character] = 36;
+    if (strcmp(city,"MI") == 0) hunterView->playerLocations[character] = 37;
+    if (strcmp(city,"MU") == 0) hunterView->playerLocations[character] = 38;
+    if (strcmp(city,"NA") == 0) hunterView->playerLocations[character] = 39;
+    if (strcmp(city,"NP") == 0) hunterView->playerLocations[character] = 40;
+    if (strcmp(city,"NU") == 0) hunterView->playerLocations[character] = 41;
+    if (strcmp(city,"PA") == 0) hunterView->playerLocations[character] = 42;
+    if (strcmp(city,"PL") == 0) hunterView->playerLocations[character] = 43;
+    if (strcmp(city,"PR") == 0) hunterView->playerLocations[character] = 44;
+    if (strcmp(city,"RO") == 0) hunterView->playerLocations[character] = 45;
+    if (strcmp(city,"SA") == 0) hunterView->playerLocations[character] = 46;
+    if (strcmp(city,"SN") == 0) hunterView->playerLocations[character] = 47;
+    if (strcmp(city,"SR") == 0) hunterView->playerLocations[character] = 48;
+    if (strcmp(city,"SJ") == 0) hunterView->playerLocations[character] = 49;
+    if (strcmp(city,"SO") == 0) hunterView->playerLocations[character] = 50;
+    if (strcmp(city,"ST") == 0) hunterView->playerLocations[character] = 51;
+    if (strcmp(city,"SW") == 0) hunterView->playerLocations[character] = 52;
+    if (strcmp(city,"SZ") == 0) hunterView->playerLocations[character] = 53;
+    if (strcmp(city,"TO") == 0) hunterView->playerLocations[character] = 54;
+    if (strcmp(city,"VA") == 0) hunterView->playerLocations[character] = 55;
+    if (strcmp(city,"VR") == 0) hunterView->playerLocations[character] = 56;
+    if (strcmp(city,"VE") == 0) hunterView->playerLocations[character] = 57;
+    if (strcmp(city,"VI") == 0) hunterView->playerLocations[character] = 58;
+    if (strcmp(city,"ZA") == 0) hunterView->playerLocations[character] = 59;
+    if (strcmp(city,"ZU") == 0) hunterView->playerLocations[character] = 60;
+
+
+    if (strcmp(city,"C?") == 0) hunterView->playerLocations[character] = 71;
+    if (strcmp(city,"S?") == 0) hunterView->playerLocations[character] = 72;
+    if (strcmp(city,"HI") == 0) hunterView->playerLocations[character] = 73;
+    if (city[0] == 'D') hunterView->playerLocations[character] = 73 + (int)city[1] - '0';
+    if (strcmp(city,"TP") == 0) hunterView->playerLocations[character] = 79;
+    
+    //Look at the remaining 4 spaces
+    
+    int i;
+    char event;
+    for (i = 0; i < 4; i ++) {
+    
+        event = play[i + 3]; //The +3 skips out the character and city code
+        
+        if (character == 4) {
+            //Dracula
+            if (event == 'T') {
+                //Placed a trap
+            }
+            if (event == 'V') {
+                //Vampire has matured
+                hunterView->score -= 13;
+            }
+        } else {
+            //Hunter
+            if (event == 'T') {
+                //Trap was encountered
+                hunterView->hp[character] -= 2;
+            } else if (event == 'V') {
+                //immature vampire is vanquished
+            } else if (event == 'D') {
+                //Dracula was encountered
+                hunterView->hp[4] -= 10; //Dracula loses 10 points
+                hunterView->hp[character] -= 4; //Hunter loses 4 points
+            }
+        }
+    }
+}
+
 // this function frees all memory previously allocated for the HunterView
 // toBeDeleted. toBeDeleted should not be accessed after the call.
 void disposeHunterView( HunterView toBeDeleted ) {

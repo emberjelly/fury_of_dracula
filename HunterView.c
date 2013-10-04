4e4543044bb088c9a97939507114c7f073663c8c
Jerryfish <bell.jezz@gmail.com>
2013-10-04 11:37:58 +1000

    Finished the edges(need checking)

--- HunterView.c
+++ HunterView.c
@@ -292,14 +292,210 @@ static void makeMap(Graph g){
     
     addLink(g, BERLIN, LEIPZIG, ROAD);
     addLink(g, BERLIN, LEIPZIG, RAIL);
-
     addLink(g, BERLIN, PRAGUE, ROAD);
     addLink(g, BERLIN, PRAGUE, RAIL);
     
-    addLink(g, BRUSSELS, PRAGUE, ROAD);
-    addLink(g, BERLIN, PRAGUE, ROAD);
+    addLink(g, BRUSSELS, LE_HAVRE, ROAD);
+    addLink(g, BRUSSELS, PARIS, ROAD);
+    addLink(g, BRUSSELS, PARIS, RAIL);
+    addLink(g, BRUSSELS, STRASBOURG, ROAD);
 
+    addLink(g, FRANKFURT, STRASBOURG, ROAD);
+    addLink(g, FRANKFURT, STRASBOURG, RAIL);
+    addLink(g, FRANKFURT, NUREMBURG, ROAD);
+    addLink(g, FRANKFURT, LEIPZIG, ROAD);
+    addLink(g, FRANKFURT, LEIPZIG, RAIL);
+    
+    addLink(g, LEIPZIG, NUREMBURG, ROAD);
+    addLink(g, LEIPZIG, NUREMBURG, RAIL);
+   
+    addLink(g, LE_HAVRE, NANTES, ROAD);
+    addLink(g, LE_HAVRE, PARIS, ROAD);
+    addLink(g, LE_HAVRE, PARIS, RAIL);
+    
+    addLink(g, PARIS, NANTES, ROAD);
+    addLink(g, PARIS, BORDEAUX, RAIL);
+    addLink(g, PARIS, CLERMONT_FERRAND, ROAD);
+    addLink(g, PARIS, MARSEILLES, RAIL);
+    addLink(g, PARIS, GENEVA, ROAD);
+    addLink(g, PARIS, STRASBOURG, ROAD);
+    
+    addLink(g, STRASBOURG, GENEVA, ROAD);
+    addLink(g, STRASBOURG, ZURICH, ROAD);
+    addLink(g, STRASBOURG, ZURICH, RAIL);
+    addLink(g, STRASBOURG, NUREMBURG, ROAD);
+    addLink(g, STRASBOURG, MUNICH, ROAD);
 
+    addLink(g, NUREMBURG, MUNICH, RAIL);
+    addLink(g, NUREMBURG, MUNICH, ROAD);
+    addLink(g, NUREMBURG, PRAGUE, ROAD);
+    
+    addLink(g, PRAGUE, VIENNA, ROAD);
+    addLink(g, PRAGUE, VIENNA, RAIL);
+   
+    addLink(g, NANTES, BORDEAUX, SEA);
+    addLink(g, NANTES, SANTANDER, SEA);
+    addLink(g, NANTES, BORDEAUX, ROAD);
+    addLink(g, NANTES, CLERMONT_FERRAND, ROAD);
+    
+    addLink(g,CLERMONT_FERRAND, BORDEAUX, ROAD);
+    addLink(g,CLERMONT_FERRAND, MARSEILLES, ROAD);
+    addLink(g,CLERMONT_FERRAND, GENEVA, ROAD);
+    addLink(g,CLERMONT_FERRAND, TOULOUSE, ROAD);
+    
+    addLink(g, GENEVA, MARSEILLES, ROAD);
+    addLink(g, GENEVA, MILAN, RAIL);
+    addLink(g, GENEVA, ZURICH, ROAD);
+    
+    addLink(g, ZURICH, MUNICH, ROAD);
+    addLink(g, ZURICH, MILAN, ROAD);
+    addLink(g, ZURICH, MILAN, RAIL);
+    addLink(g, ZURICH, MARSEILLES, ROAD);
+    
+    addLink(g, MUNICH, MILAN, ROAD);
+    addLink(g, MUNICH, VENICE, ROAD);
+    addLink(g, MUNICH, ZAGREB, ROAD);
+    addLink(g, MUNICH, VIENNA, ROAD);
+    
+    addLink(g, BORDEAUX, SANTANDER, SEA);
+    addLink(g, BORDEAUX, SARAGOSSA, ROAD);
+    addLink(g, BORDEAUX, SARAGOSSA, RAIL);
+    addLink(g, BORDEAUX, TOULOUSE, ROAD);
+    
+    addLink(g, TOULOUSE, SARAGOSSA, ROAD);
+    addLink(g, TOULOUSE, BARCELONA, ROAD);
+    addLink(g, TOULOUSE, MARSEILLES, ROAD);
+    
+    addLink(g, MARSEILLES, MILAN, ROAD);
+    addLink(g, MARSEILLES, GENOA, ROAD);
+    addLink(g, MARSEILLES, BARCELONA, SEA);
+    addLink(g, MARSEILLES, ALICANTE, SEA);
+    addLink(g, MARSEILLES, CAGLIARI, SEA);
+
+    addLink(g, MILAN, GENOA, ROAD);
+    addLink(g, MILAN, VENICE, ROAD);
+    addLink(g, MILAN, FLORENCE, RAIL);
+    
+    addLink(g, VENICE, FLORENCE, ROAD);
+    addLink(g, VENICE, VIENNA, RAIL);
+    addLink(g, VENICE, BARI, SEA);
+    addLink(g, VENICE, GENOA, ROAD);
+    
+    addLink(g, GENOA, FLORENCE, ROAD);
+    addLink(g, GENOA, ROME, SEA);
+    addLink(g, GENOA, NAPLES, SEA);
+    addLink(g, GENOA, CAGLIARI, SEA);
+    
+    addLink(g, FLORENCE, ROME, ROAD);
+    addLink(g, FLORENCE, ROME, RAIL);
+    
+    addLink(g, SANTANDER, LISBON, ROAD);
+    addLink(g, SANTANDER, MADRID, ROAD);
+    addLink(g, SANTANDER, MADRID, RAIL);
+    addLink(g, SANTANDER, SARAGOSSA, ROAD);
+    
+    addLink(g, LISBON, CADIZ, SEA);
+    addLink(g, LISBON, CADIZ, ROAD);
+    addLink(g, LISBON, MADRID, ROAD);
+    addLink(g, LISBON, MADRID, RAIL);
+    
+    addLink(g, MADRID, SARAGOSSA, RAIL);
+    addLink(g, MADRID, SARAGOSSA, ROAD);
+    addLink(g, MADRID, ALICANTE, RAIL);
+    addLink(g, MADRID, ALICANTE, ROAD);
+    addLink(g, MADRID, GRANADA, ROAD);
+    addLink(g, MADRID, CADIZ, ROAD);
+    
+    addLink(g, CADIZ, GRANADA, ROAD);
+
+    addLink(g, GRANADA, ALICANTE, ROAD);
+
+    addLink(g, ALICANTE, SARAGOSSA, ROAD);
+    addLink(g, ALICANTE, BARCELONA, RAIL);
+    addLink(g, ALICANTE, BARCELONA, SEA);
+    addLink(g, ALICANTE, GENOA, SEA);
+    addLink(g, ALICANTE, CAGLIARI, SEA);
+    
+    addLink(g, CAGLIARI, ROME, SEA);
+    addLink(g, CAGLIARI, NAPLES, SEA);
+    
+    addLink(g, SARAGOSSA, BARCELONA, ROAD);
+    addLink(g, SARAGOSSA, BARCELONA, RAIL);
+
+    addLink(g, ROME, NAPLES, SEA);
+    addLink(g, ROME, NAPLES, ROAD);
+    addLink(g, ROME, NAPLES, RAIL);
+    addLink(g, ROME, BARI, ROAD);
+    
+    addLink(g, NAPLES, BARI, ROAD);
+    addLink(g, NAPLES, BARI, RAIL);
+    
+    addLink(g, VIENNA, BUDAPEST, RAIL);
+    addLink(g, VIENNA, BUDAPEST, ROAD);
+    addLink(g, VIENNA, ZAGREB, ROAD);
+    
+    addLink(g, BUDAPEST, ZAGREB, ROAD);
+    addLink(g, BUDAPEST, SZEGED, ROAD);
+    addLink(g, BUDAPEST, SZEGED, RAIL);
+    addLink(g, BUDAPEST, KLAUSENBURG, ROAD);
+    
+    addLink(g, KLAUSENBURG, CASTLE_DRACULA, ROAD);
+    addLink(g, KLAUSENBURG, SZEGED, ROAD);
+    addLink(g, KLAUSENBURG,BELGRADE, ROAD);
+    addLink(g, KLAUSENBURG, GALATZ, ROAD);
+    addLink(g, KLAUSENBURG, BUCHAREST, ROAD);
+    
+    addLink(g, GALATZ, CASTLE_DRACULA, ROAD);
+    addLink(g, GALATZ, BUCHAREST, ROAD);
+    addLink(g, GALATZ, BUCHAREST, RAIL);
+    addLink(g, GALATZ, CONSTANTA, ROAD);
+    
+    addLink(g, CONSTANTA, BUCHAREST, ROAD);
+    addLink(g, CONSTANTA, BUCHAREST, RAIL);
+    addLink(g, CONSTANTA, VARNA, ROAD);
+    addLink(g, CONSTANTA, VARNA, SEA);
+    addLink(g, CONSTANTA, SALONICA, SEA);
+    addLink(g, CONSTANTA, ATHENS, SEA);
+    addLink(g, CONSTANTA, VALONA, SEA);
+    
+    addLink(g, ZAGREB, SZEGED, ROAD);
+    addLink(g, ZAGREB, SARAJEVO, ROAD);
+    addLink(g, ZAGREB, ST_JOSEPH_AND_ST_MARYS, ROAD);
+
+    addLink(g, ST_JOSEPH_AND_ST_MARYS, SZEGED, ROAD);
+    addLink(g, ST_JOSEPH_AND_ST_MARYS, SARAJEVO, ROAD);
+    addLink(g, ST_JOSEPH_AND_ST_MARYS, BELGRADE, ROAD);
+    
+    addLink(g, SZEGED, BELGRADE, ROAD);
+    addLink(g, SZEGED, BELGRADE, RAIL);
+    addLink(g, SZEGED, BUCHAREST, RAIL);
+    
+    addLink(g, BUCHAREST, BELGRADE, ROAD);
+    addLink(g, BUCHAREST, SOFIA, ROAD);
+    
+    addLink(g, VARNA, SALONICA, SEA);
+    addLink(g, VARNA, ATHENS, SEA);
+    addLink(g, VARNA, VALONA, SEA);
+    
+    addLink(g, SARAJEVO, BELGRADE, ROAD);
+    addLink(g, SARAJEVO, SOFIA, ROAD);
+    addLink(g, SARAJEVO, VALONA, ROAD);
+    
+    addLink(g, BELGRADE, SOFIA, ROAD);
+    addLink(g, BELGRADE, SOFIA, RAIL);
+    
+    addLink(g, SOFIA, VALONA, ROAD);
+    addLink(g, SOFIA, VARNA, ROAD);
+    addLink(g, SOFIA, VARNA, RAIL);
+    addLink(g, SOFIA, SALONICA, RAIL);
+    addLink(g, SOFIA, SALONICA, ROAD);
+    
+    addLink(g, VALONA, SALONICA, ROAD);
+    addLink(g, VALONA, SALONICA, SEA);
+    addLink(g, VALONA, ATHENS, SEA);
+    addLink(g, VALONA, ATHENS,ROAD);
+    
+    addLink(g, ATHENS, SALONICA, SEA);
 }
 
 //Useful for debugging
@@ -312,9 +508,11 @@ static void showGraph(Graph g) {
         while (n != NULL) { 
             printf("%d-%d ",i,n->v); 
             if(n->type == ROAD){
-                printf("L ");
+                printf("R ");
             } else if(n->type == SEA){
                 printf("S ");
+            } else if (n->type == RAIL) {
+                printf("RA ");
             } else {
                 printf("ERROR NO SUCH TYPE");
                 exit(0);
